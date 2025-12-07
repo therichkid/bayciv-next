94a21970e810c3d783b89837844c4f63883be793
Richard Zille <therichkid@gmx.de>
2025-12-07 15:57:53 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+	import Navigation from '$lib/components/Navigation.svelte';
+	import { ModeWatcher } from 'mode-watcher';
+	import './layout.css';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher />
+
+<Navigation />
+
+{@render children()}
